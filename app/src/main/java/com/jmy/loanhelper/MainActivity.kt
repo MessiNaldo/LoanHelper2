5ebdd84caf6f8ee70a206976dabc6600816262b0
James Jung <jminyong81@gmail.com>
2022-06-28 17:43:24 +0900

    test

--- app/src/main/java/com/jmy/loanhelper/MainActivity.kt
+++ app/src/main/java/com/jmy/loanhelper/MainActivity.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.launch
 import java.lang.Thread.sleep
 import kotlin.concurrent.thread
 
+//test
 class MainActivity : AppCompatActivity() {
     val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     var bundle = Bundle()
